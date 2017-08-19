805eb1e3a7eed650879e46891d475bfee5827805
Wilk Maia <wilkmaia@gmail.com>
2017-08-19 15:07:15 -0300

    wip

--- app/models/flows.js
+++ app/models/flows.js
@@ -3,29 +3,29 @@
 const Sequelize = require('sequelize')
 
 module.exports = (sequelize) => {
-	const Flows = sequelize.define('Flows', {
-		id: {
-			type: Sequelize.INTEGER,
-			autoIncrement: true,
-			primaryKey: true,
-		},
-		order: {
-			type: Sequelize.ARRAY(Sequelize.INTEGER),
-			allowDefault: false,
-		},
-		createdAt: {
+  const Flows = sequelize.define('Flows', {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    order: {
+      type: Sequelize.ARRAY(Sequelize.INTEGER),
+      allowDefault: false,
+    },
+    createdAt: {
       type: Sequelize.DATE,
     },
     updatedAt: {
       type: Sequelize.DATE,
     },
-	})
+  })
 
-	Flows.associate = (models)	=> {
-		Flows.hasMany(models.Blocks)
-	}
+  Flows.associate = (models)  => {
+    Flows.hasMany(models.Blocks)
+  }
 
-	return Flows
+  return Flows
 }
 
 
