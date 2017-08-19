9aa9e5ad665ad8e6a53fedfa9ba154c8fc6ce11a
Wilk Maia <wilkmaia@gmail.com>
2017-08-19 11:26:27 -0300

    Remove test log

--- app/helpers/full_contact.js
+++ app/helpers/full_contact.js
@@ -4,8 +4,6 @@ const axios = require('axios')
 require('dotenv').config()
 
 async function find (email) {
-  console.log(email)
-
   return await axios({
     method: 'get',
     url: `https://api.fullcontact.com/v2/person.json?email=${email}&macromeasures=true`,
