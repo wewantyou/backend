adb714def889c585e2500bd259d6895e72437233
Wilk Maia <wilkmaia@gmail.com>
2017-08-19 00:58:59 -0300

    Add database connection

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 'use strict'
 
 const express = require('express')
-const sequelize = require('sequelize')
+const Sequelize = require('sequelize')
 const bodyParser = require('body-parser')
 
 const app = express()
 const port = 8080
-const dbAddr = 'localhost'
+
+const database = 'wewantyou'
+const dbUsername = 'root'
+const dbPassword = ''
+const dbHost = 'localhost'
+const dbDialect = 'mysql'
+const sequelize = new Sequelize(database, dbUsername, dbPassword, {
+  host: dbHost,
+  dialect: dbDialect,
+})
 
 const candidatesRoutes = require('./app/routes/candidates')
 
@@ -14,6 +23,14 @@ app.use(bodyParser.json())
 
 candidatesRoutes(app)
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+sequelize
+  .authenticate()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`)
+    })
+  })
+  .catch((err) => {
+    console.log(`Errored: ${err}`)
+    sequelize.close()
+  })
