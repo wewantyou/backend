d513b9384b92d7ec4b5d5a2296ea2df8859b863b
rodrigoPgSup <rodrigo.amaral@pagar.me>
2017-08-19 03:54:23 -0300

    add fullcontact api helper and function create for candidate

--- app/controllers/candidates.js
+++ app/controllers/candidates.js
@@ -1,9 +1,30 @@
 'use strict'
 
+const fullContactHelper = require('../helpers/fullContact')
+
 class CandidatesController {
   listAll (req, res) {
     res.send()
   }
+    create(req, res, models) {
+        models.Candidates.create({
+            name: req.body.name,
+            fullContactProfile: fullContactHelper.find(req.body.email),
+            email: req.body.email
+        })
+            .then((candidate) => {
+                res.json({
+                    error: false,
+                    id: candidate.id
+                })
+            })
+            .catch((err) => {
+                res.json({
+                    error: true,
+                    message: err
+                })
+            })
+    }
 }
 
 module.exports = CandidatesController
---
+++ app/helpers/full_contact.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const axios = require('axios')
+require('dotenv').config()
+
+async function find (email) {
+    return await axios({
+        method: 'get',
+        url: `https://api.fullcontact.com/v2/person.json?email=${email}&macromeasures=true`,
+        headers: {'X-FullContact-APIKey': process.env.FULL_CONTACT_API_KEY}
+    })
+}
+
